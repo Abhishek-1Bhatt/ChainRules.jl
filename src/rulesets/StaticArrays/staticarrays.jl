52e66e474ee421b3f024251bf95cd800924f6ced
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2022-07-28 07:24:05 +0530

    update src/staticarrays.jl

--- src/rulesets/StaticArrays/staticarrays.jl
+++ src/rulesets/StaticArrays/staticarrays.jl
@@ -2,14 +2,14 @@
 ##### constructors
 #####
 
-ChainRules.@non_differentiable (::Type{T} where {T<:SArray})(::UndefInitializer, args...)
+ChainRules.@non_differentiable (::Type{T} where {T<:Union{SArray, SizedArray}})(::UndefInitializer, args...)
 
-function frule((_, ẋ), ::Type{T}, x::AbstractArray) where {T<:SArray}
-    return T(x), T(ẋ)
+function frule((_, ẋ), ::Type{T}, x::AbstractArray) where {T<:Union{SArray, SizedArray}}
+    return SArray(x), SArray(ẋ)
 end
 
-function rrule(::Type{T}, x::AbstractArray) where {T<:SArray}
+function rrule(::Type{T}, x::AbstractArray) where {T<:Union{SArray, SizedArray}}
     project_x = ProjectTo(x)
     Array_pullback(ȳ) = (NoTangent(), project_x(ȳ))
-    return T(x), Array_pullback
+    return SArray(x), Array_pullback
 end
